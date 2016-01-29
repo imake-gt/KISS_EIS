74f465a9f26078abbe50ebd1fb4d09eaa4174154
IMake <moooooooooooogle@gmail.com>
2016-01-30 00:44:51 +0700

    init Converter

---
+++ ChartConverter/src/main/java/th/co/imake/chart/services/ConverterServices.java
@@ -0,0 +1,7 @@
+package th.co.imake.chart.services;
+
+/**
+ * Created by imake on 30/01/2016.
+ */
+public interface ConverterServices {
+}
---
+++ ChartConverter/src/main/java/th/co/imake/chart/services/impl/ConverterImpl.java
@@ -0,0 +1,7 @@
+package th.co.imake.chart.services.impl;
+
+/**
+ * Created by imake on 30/01/2016.
+ */
+public class ConverterImpl {
+}
